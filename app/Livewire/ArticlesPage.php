734a4d59c0f8ac28c36ec7e8e63d003781e4dd38
u226 <me@gmail.com>
2024-10-21 14:47:34 +0330

    Mounting!

--- app/Livewire/ArticlesPage.php
+++ app/Livewire/ArticlesPage.php
@@ -2,10 +2,29 @@
 
 namespace App\Livewire;
 
+use Carbon\Carbon;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Title('Properties / Articles')]
 class ArticlesPage extends Component
 {
-    public $description = 'You are viewing your articles';
+    public $isTimerOn;
+    public $description;
+
+    public function changeDescription()
+    {
+        $this->description = 'Changed';
+    }
+
+    public function mount()
+    {
+        $this->isTimerOn = true;
+        $this->description = 'You are viewing your articles at ' . Carbon::now()->format('d F Y - H:i:s');
+
+        $this->fill([
+            'isTimerOn' => true,
+            'description' => 'Hello, Im new description',
+        ]);
+    }
 }
--- resources/views/livewire/articles-page.blade.php
+++ resources/views/livewire/articles-page.blade.php
@@ -5,4 +5,13 @@
     <p>
         {{ $description }}
     </p>
+        Timer is
+    @if($isTimerOn)
+        <strong>ON</strong>
+        <br>
+        time is <strong wire:poll.keep-alive> {{ time() }}</strong>
+        <button wire:click="changeDescription">Change description</button>
+    @else
+        <strong>OFF</strong>
+    @endif
 </div>
