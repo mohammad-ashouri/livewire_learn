fb83dc56f349410faa8dbc439e719958d4b62e9b
u226 <me@gmail.com>
2024-11-10 16:23:01 +0330

    Make project blank

--- resources/views/livewire/task-row.blade.php
+++
@@ -1,6 +0,0 @@
-<li>
-    {{ $taskValue }}
-    <button wire:click="$parent.deleteTask('{{ $taskKey }}')">
-        Delete
-    </button>
-</li>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-use App\Livewire\Counter;
-use App\Livewire\Event;
-use App\Livewire\Export;
-use App\Livewire\MagicActions;
-use App\Livewire\TodoList;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => view('welcome'))->name('page.welcome');
-Route::get('counter', Counter::class)->name('page.counter');
-Route::get('export', Export::class)->name('page.export');
-Route::get('event', Event::class)->name('page.event');
-Route::get('magics', MagicActions::class)->name('page.magics');
-Route::get('todo-list', TodoList::class)->name('page.todo-list');
