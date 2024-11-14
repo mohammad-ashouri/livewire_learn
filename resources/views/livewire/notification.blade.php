8c10921861947b90645e7755a30f4360f094d987
u226 <me@gmail.com>
2024-11-14 13:34:53 +0330

    Dispatch with alpine

--- resources/views/livewire/notification.blade.php
+++ resources/views/livewire/notification.blade.php
@@ -1,14 +1,40 @@
 <div>
     @if($is_visible)
         <div
-            style="position: absolute; top: 2rem; right: 2rem; background-color: #656565; border-radius: 0.2rem; padding: 0.6rem 0.4rem; max-width: 24rem; width: 100%; text-align: right">
+            style="position: absolute;
+             top: 2rem;
+              right: 2rem;
+               background-color: #656565;
+                border-radius: 0.2rem;
+                 padding: 0.6rem 0.4rem;
+                  max-width: 24rem;
+                   width: 100%;
+                    text-align: right;
+                     display: flex;
+                     flex-direction: row-reverse;
+                      align-items: center;
+                      justify-content: space-between;
+                    ">
+        <span>
             {{ $message }}
+        </span>
+            <span
+                style="width: 12px; height: 12px; padding: 4px; border-radius: 0.2rem; background-color: #414141; display: flex; align-items: center; justify-content: center; cursor: pointer"
+                @click="$wire.dispatch('hide')"
+            >
+            *
+        </span>
         </div>
     @endif
 </div>
 @script
 <script>
     let tid = null;
+    $wire.on('hide', () => {
+        clearTimeout(tid);
+        $wire.set('is_visible', false);
+    });
+
     $wire.on('hide-notification', () => {
         clearTimeout(tid);
         tid = setTimeout(
@@ -17,7 +43,9 @@
             },
             3000
         )
-    })
-
+    });
+    Livewire.on('notify', () => {
+        console.log('notify called from another component')
+    });
 </script>
 @endscript
